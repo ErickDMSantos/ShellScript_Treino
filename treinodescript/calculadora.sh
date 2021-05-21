1fd364908d81ad8a7a3c17e4c09de1bac85e144b
Erick Santos <erick.santos@liferay.com>
2021-05-21 10:20:44 -0300

    shell

--- treinodescript/calculadora.sh
+++ treinodescript/calculadora.sh
@@ -14,6 +14,7 @@ adicao() {
                             adicao
                         elif [[ $ad2 = +(0|1|2|3|4|5|6|7|8|9) ]];then
                             echo -e " Resultado e: $(($ad1+$ad2)) \n====================================="
+                            sleep 1
                         fi    
             fi
          
@@ -31,6 +32,7 @@ subtracao() {
                             subtracao
                         elif [[ $sub2 = +(0|1|2|3|4|5|6|7|8|9) ]];then
                             echo -e " Resultado e: $(($sub1-$sub2)) \n====================================="
+                            sleep 1
                         fi
             fi
           
@@ -47,6 +49,7 @@ multiplicacao() {
                             multiplicacao
                         elif [[ $mult2 = +(0|1|2|3|4|5|6|7|8|9) ]];then
                             echo -e " Resultado e: $(expr $mult1 \* $mult2) \n====================================="
+                            sleep 1
                         fi
             fi
        
@@ -63,6 +66,7 @@ divisao() {
                             divisao
                         elif [[ $div2 = +(0|1|2|3|4|5|6|7|8|9) ]];then
                             echo -e " Resultado e: $(expr $div1 / $div2) \n====================================="
+                            sleep 1
                         fi
             fi
         
@@ -71,7 +75,7 @@ divisao() {
 
 Calculo() {
 	  echo -en "\n Qual a operacao voce deseja fazer? 
-        \n 0- Nao Quero mais fazer calculos 
+        \n 0- Sair
             \n 1- Adicao
                 \n 2- Subtracao
                     \n 3- Multiplicacao
